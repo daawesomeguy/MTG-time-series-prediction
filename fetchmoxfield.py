da693de41995f31801652aa11f97aefc79e71d99
jpeppercorn16 <jpeppercorn16@gmail.com>
2025-01-30 22:30:47 -0500

    moxfield code

--- fetchmoxfield.py
+++ fetchmoxfield.py
@@ -1,1 +1,11 @@
+import requests
 
+r = requests.get('https://moxfield.com/decks/public', user)
+
+def save_html(html, path):
+    with open(path, 'wb') as f:
+        f.write(html)
+
+def open_html(path):
+    with open(path, 'rb') as f:
+        return f.read()
