5dbe907162fb6ba8e03a96c027f5d39f46194a52
Jack Nugent <83517345+daawesomeguy@users.noreply.github.com>
2025-01-31 12:34:12 -0500

    Update fetcharchideckt.py

--- fetcharchideckt.py
+++ fetcharchideckt.py
@@ -1,34 +1,37 @@
-import requests
+import requests, json
 from bs4 import BeautifulSoup
-import re, json
-r=requests.get('https://archidekt.com/decks/10260015/621_wtf_are_you_doing')
-soup = BeautifulSoup(r.text)
-print(soup)
-#pattern = r'"name":\s*"([^"]+)"'
-#matches = re.findall(pattern, r.text)
-
-#if matches:
- #   print(len(matches))
-#    #print("Matches:", matches)
-script_tag = soup.find("script", {"id": "__NEXT_DATA__"})
-
-if script_tag:
-    json_data = json.loads(script_tag.string)  # Convert to Python dict
-    #print(json.dumps(json_data, indent=4))  # Pretty print JSON
-else:
-    print("JSON data not found!")
 from functools import reduce
-
-# Function to extract a nested value
 def get_value(data, path):
-    keys = path.split(".")
-    return reduce(lambda d, key: d[int(key)] if isinstance(d, list) and key.isdigit() else d.get(key, None), keys, data)
+        keys = path.split(".")
+        return reduce(lambda d, key: d[int(key)] if isinstance(d, list) and key.isdigit() else d.get(key, None), keys, data)
 
-# Example Usage
-result = get_value(json_data, "props.pageProps.redux.deck.cardMap")
-#print(result)
-names = [entry["name"] for entry in result.values()if "name" in entry]
+def get_links():
+    r=requests.get('https://archidekt.com/search/decks?deckFormat=3&orderBy=-updatedAt&page=1')
+    soup = BeautifulSoup(r.text)
+    #print(soup)
+    script_tag=soup.find(class_='deckLink_thumbnail__U3ZsF')
+    print(str(script_tag))
+    if script_tag and script_tag.has_attr('href'):
+        link = "https://archidekt.com" + script_tag['href']
+    print(link)
+    return link
+def get_cards(link):
+    r=requests.get(link)
+    soup = BeautifulSoup(r.text)
+    script_tag = soup.find("script", {"id": "__NEXT_DATA__"})
+    if script_tag:
+        json_data = json.loads(script_tag.string) 
+    else:
+        print("JSON data not found!")
+    result = get_value(json_data, "props.pageProps.redux.deck.cardMap")
+    names = [entry["name"] for entry in result.values()if "name" in entry]
+    return (names)
 
-print(names)
-#with open('text.txt','w') as file:
-#    file.write(soup.prettify())
+
+#for tag in script_tag:
+    #print(tag.find('href=').text)
+#if script_tag:
+    #json_data = json.loads(str(script_tag))  # Convert to Python dict
+    #print(json.dumps(json_data, indent=4))
+
+print(get_cards(get_links()))
