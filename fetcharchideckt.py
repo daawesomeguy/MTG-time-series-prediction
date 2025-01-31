392c0690d16c545d8d5e4000a2ba0377cd71bb74
Jack Nugent <83517345+daawesomeguy@users.noreply.github.com>
2025-01-30 22:32:59 -0500

    Update fetcharchideckt.py

--- fetcharchideckt.py
+++ fetcharchideckt.py
@@ -1,1 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+r=requests.get('https://archidekt.com/decks/11143456/history_matter')
+soup=soup = BeautifulSoup(r.text, 'html.parser')
+
+print(soup.prettify())
 
